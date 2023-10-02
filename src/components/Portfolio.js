7c9e8647ca14ff9367150c04f5d38436da24353d
nandha-techie <nandhawebapps@gmail.com>
2023-10-02 10:33:11 +0530

    react admin portfolio added

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,6 @@
 import Profile from '../images/car_rental.png'; 
 import Shopping_img from '../images/shopping_site.png';
+import React_admin_panel from '../images/react_admin-panel.png';
 import '../styles/portfolio.css';
 
 const Protfolio = ()=>{
@@ -16,7 +17,7 @@ const Protfolio = ()=>{
                             <a href="https://nandha-techie.github.io/car-rental/" target="__blank"><img src={Profile} alt="portfolio box" /></a>
                         </div>
                         <div className="portfolio__box">
-                            <a href="https://nandha-techie.github.io/React-shoppingcard/" target="__blank"><img src={Shopping_img} alt="portfolio box" /></a>
+                            <a href="https://nandha-techie.github.io/react-admin-panel/" target="__blank"><img src={React_admin_panel} alt="portfolio box" /></a>
                         </div>
                     </div>
                 </div>
